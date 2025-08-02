22e486af7fb145957b7b931ef31256b410c2316f
Geremi57 <geremiwanga57@gmail.com>
2025-08-02 08:25:19 +0300

    hard to tackle

---
+++ quad/QuadA.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func QuadA(x, y int){
+if x > 0 && y > 0 {
+		fmt.Print("o")
+		if  x > 1 {
+		for i:=0; i < x -2;i++{
+			fmt.Printf("-")
+		}
+		fmt.Println("o")
+	}else {
+		fmt.Println()
+	}
+		for i:=0; i < y-2; i++{
+			fmt.Print("|")
+		if x > 1{
+			for k:=0; k < x-2; k++{
+			fmt.Print(" ")
+			}
+			fmt.Println("|")
+		}else{ fmt.Println()}
+	}
+	if y > 1 {
+fmt.Print("o")
+		if x > 1 {
+		for i:=0; i < x -2;i++{
+			fmt.Print("-")
+		}
+		fmt.Println("o")
+	}
+		}else {
+			fmt.Println()
+		}
+}
+}
+
+func main() {
+	QuadA(5, 5)
+}
