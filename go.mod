7a891e52caf556435ba96bfb1853b7c184a2de37
Geremi57 <geremiwanga57@gmail.com>
2025-08-02 06:18:50 +0300

    run 'go fmt prtstr.go' to file to format

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test2.go
+
+go 1.24.5
+
+require github.com/01-edu/z01 v0.1.0 // indirect
---
+++ test.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i  = 16
+	numb := 18.9
+	text := "water"
+	  fmt.Printf("%v\n", i)
+	  fmt.Printf("%#v\n", i)
+	  fmt.Printf("%T\n", text)
+	  fmt.Printf("%T\n", numb)
+
+	  fmt.Printf("%#v\n", i)
+}
