bdb523cae2e7009955efb9ef58ded1a74b39d657
gwanga <wangageremi725@gmail.com>
2025-08-19 11:57:51 +0300

    slice in slice

--- sst/chunk.go
+++ sst/chunk.go
@@ -3,13 +3,28 @@ package main
 import "fmt"
 
 func Chunk(slice []int, size int) {
-if size == 0 {
-	return fmt.Println()
-}
+	if size <= 0 {
+		fmt.Println()
+		return 
+	}
+	var mainS [][]int
 
+for i:=0; i < len(slice); i += size{
+
+	end := i + size
+	if end > len(slice) {
+		end = len(slice)
+	}
+
+	Chunk:= slice[i:end]
+
+	mainS = append(mainS, Chunk)
+}
+fmt.Println(mainS)
 
 }
 
 func main() {
-
+	sl := [] int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	Chunk(sl, 0)
 }
