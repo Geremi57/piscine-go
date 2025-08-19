3bf45584c6b5b33d02f5f104033970846eaeff0b
gwanga <wangageremi725@gmail.com>
2025-08-19 19:01:52 +0300

    almost

---
+++ sst/fifthandskip.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func FifthAndSkip(str string) string{
+	final:= ""
+
+	for _, value:= range str{
+		if value  >= 'A' && value <= 'Z' || value >= 'a' && value <= 'z' || value >= '0' && value  <= '9'{
+				final += string(value)
+		}
+	}
+	s:=""
+	for i:=0; i < len(final); i++{
+		s += string(final[i])
+		if i != 1 {
+			if i % 5 == 0 {
+				s += " "
+			}
+		}
+	
+	
+	}
+	return s
+
+}
+
+func main() {
+	fmt.Println(FifthAndSkip("abcd efgh ijkl mnop qrst"))
+}
