94ec5a9ea2d66f51c7f36aa2501bc85fdbf663dd
gwanga <wangageremi725@gmail.com>
2025-08-23 18:35:58 +0300

    everything

--- sst/saveandmiss.go
+++ sst/saveandmiss.go
@@ -1,34 +1,32 @@
 package main
-
 import "fmt"
 
 func saveandmiss(s string, num int) string{
+	fin := ""
 	arr := []string{}
-	
-	for i:=0; i < len(s); i += num{
+	for i:=0; i < len(s); i += num {
 		end := i + num
-		if end > len(s) {
+		if len(s) < end {
 			end = len(s)
 		}
-		arr = append(arr, s[i : end])
-	}
+		arr = append(arr, s[i:end])
 
-	final := ""
+		// if i % 3 == 0 {
+		// 	end = i
+		// }
+		// fin = s[i:end]
+	}
 	for i,value := range arr{
-		if i % 2 == 0 {
-
-			final += value
-		}
+		if i % 2 == 0{
+			fin += value
+		} 
 	}
-	return final
-
+	// fmt.Println(arr)
+	return fin
 }
 
 func main(){
-	arg1 := "123456789"
-	arg2 := "abcdefghijklmnopqrstuvwxyz"
-
-	fmt.Println(saveandmiss(arg1, 3))
-	fmt.Println(saveandmiss(arg2, 3))
-
+a := "what is your name"
+fmt.Println(saveandmiss(a, 3))
 }
+
